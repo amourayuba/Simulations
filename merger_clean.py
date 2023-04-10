5d436b0d92fabeef2258ad8a4efaacf188d83a5f
amourayuba <amourayuba@gmail.com>
2023-04-10 16:07:47 -0400

    Added make zxs

--- merger_clean.py
+++ merger_clean.py
@@ -1,6 +1,5 @@
 from merger_rate import *
-from astropy.io import ascii
-from astropy.table import Table, Column, MaskedColumn
+from scipy.optimize import curve_fit
 from astropy.cosmology import LambdaCDM, z_at_value
 from astropy import units as u
 import os
@@ -19,6 +18,9 @@ plt.rcParams.update(params)
 ################ TOTAL NUMBER OF MERGERS ################################################
 #############--------------- MxSy Simulations --------------#############################
 
+def mcbride_mah(z, gamma, beta):
+    return (1 + z) ** beta * np.exp(-gamma * z)
+
 
 def get_mrat(pgmasses, wpos=False, m=None, pos_s=None):
     if wpos:
@@ -30,6 +32,7 @@ def get_mrat(pgmasses, wpos=False, m=None, pos_s=None):
     else:
         pgratios = pgmasses / np.max(pgmasses)
         return pgratios[np.where(pgratios < 1)]
+
 
 def large_growth_analytical(Mass, zs, s8, om, nxibins=10000):
     res_ana = []
@@ -66,14 +69,15 @@ def average_growth_analytical(Mass, zs, s8, om, nxibins=10000):
 
 
 def get_zlastdyn(zf, h, om, zbins=20):
-    cosmo = LambdaCDM(H0=100 * h, Om0=om, Ode0=1 - om)
+    cosmol = LambdaCDM(H0=100 * h, Om0=om, Ode0=1 - om)
     infall = 1.44 / hubble_ratio(zf, omega_m0=om, omega_l0=1 - om)  # infall time in gyr
-    ages = cosmo.age(zf).value
+    ages = cosmol.age(zf).value
     last_tdyn = ages - np.sqrt(2) * infall
-    zi = z_at_value(cosmo.age, last_tdyn * u.Gyr)
+    zi = z_at_value(cosmol.age, last_tdyn * u.Gyr)
     zs = np.linspace(zi, zf, zbins)
     dz = (zi - zf) / zbins
     return zs.value, dz.value
+
 
 class Simulation:
     """A class of objects referring to a Dark Matter only simulation"""
@@ -83,15 +87,17 @@ class Simulation:
         self.om0 = om0  # Value of Omega_m for the simulation
         self.sig8 = sig8  # Value of sigma_8
         self.path = path  # The base (absolute) path of that simulation. All simulation paths have to be structured the same.
+        self.simpath = self.path + self.name
+    #################----------------GENERIC INFORMATION OF THE SIMULATION---------------------#########################
 
     def get_redshifts(self):
         """Gets the list of redshifts of each snapshot"""
         if self.name[0] == 'M':
-            return np.loadtxt(self.path + '/redshifts/M03S08.txt')
-        elif os.path.exists(self.path + '/redshifts/{}.txt'.format(self.name)):
-            return np.loadtxt(self.path + '/redshifts/{}.txt'.format(self.name))
+            return np.loadtxt(self.path + 'redshifts/M03S08.txt')
+        elif os.path.exists(self.path + 'redshifts/{}.txt'.format(self.name)):
+            return np.loadtxt(self.path + 'redshifts/{}.txt'.format(self.name))
         else:
-            return np.loadtxt(self.path + '/redshifts/mxsy_reds.txt')[::-1]
+            return np.loadtxt(self.path + 'redshifts/mxsy_reds.txt')
 
     def get_prefs(self):
         """Gets the list of prefixes of each file for all available snapshots"""
@@ -105,16 +111,53 @@ class Simulation:
         return pd.read_table(self.path + self.name + '/halos/' + prefs[snapshot] + '.AHF_halos', delim_whitespace=True,
                              header=0)
 
-    def make_mah(self, save=False):
+    def get_subfrac(self, snapshot=0):
+        """Gives the substructure fraction of all halos at the given snapshot
+        :returns array (nhalo, 4) with (halo_idx, halo_mass, sub fraction, n subs)"""
+        prefs = self.get_prefs()
+        with open(self.path + self.name + '/substructure/' + prefs[snapshot] + '.AHF_substructure') as file:
+            lines = file.read().splitlines()
+        for i in range(len(lines)):
+            lines[i] = lines[i].split()
+        halo_nsubs = lines[::2]
+        subs_list = lines[1::2]
+
+        halos = self.read_halos(snapshot).set_index('#ID(1)')
+        nh = len(halo_nsubs)
+        subfrac = np.zeros((nh, 4))
+        for i in range(nh):
+            idx = int(halo_nsubs[i][0])
+            mhalo = halos.loc[idx]['Mhalo(4)']
+            subfrac[i, 0] = idx
+            subfrac[i, 1] = mhalo
+            msubs = 0
+
+            for sub in subs_list[i]:
+                try:  # sometimes the halo hasn't been recorded in _halos file, these cases are very rare
+                    msubs += halos.loc[int(sub)]['Mhalo(4)']
+                except KeyError:
+                    msubs += 1e12  # this is the minimum halo mass
+            subfrac[i, 2] = msubs / mhalo
+            subfrac[i, 3] = int(int(halo_nsubs[i][1]))
+        return subfrac
+
+    ###################-----------------MASS ACCRETION HISTORY RELATED QUANTITIES----------------#######################
+    def make_mah(self, save=False, fsizelim=1500):
         mahs, ids, emptyfiles = [], [], []
         filenames = os.listdir(self.path + self.name + '/mahs')
         filenames.sort()
         for file in filenames:
-            if os.path.getsize(self.path + self.name + '/mahs/' + file) > 15000:  # check that file is has actual halos
+            if os.path.getsize(
+                    self.path + self.name + '/mahs/' + file) > fsizelim:  # check that file is has actual halos
+                #mahs.append(np.loadtxt(self.path + self.name + '/mahs/' + file)[:, 4])
+                #ids.append(np.loadtxt(self.path + self.name + '/mahs/' + file)[:, 1][0])
                 mahs.append(np.loadtxt(self.path + self.name + '/mahs/' + file)[:, 4])
-                ids.append(np.loadtxt(self.path + self.name + '/mahs/' + file)[:, 1][0])
+                with open(self.path + self.name + '/mahs/' + file, 'r') as f:
+                    for _ in range(3):
+                        l = f.readline()
+                    ids.append((int(l.split()[1])))
             else:
-                print('file ' + file + ' is empty')
+                # print('file '+file+' is empty and has size ',os.path.getsize(self.path+self.name+'/mahs/'+file)/1024,'Kb')
                 emptyfiles.append(file)
         if save:
             np.save(self.path + self.name + '/mahs_{}.npy'.format(self.name), np.array(mahs, dtype=object))
@@ -130,6 +173,102 @@ class Simulation:
         """Gets the associated halo ids of the Mass Accretion History in the form of an .npy file with Nhalos
         elements, each one being a list of masses starting from z=0 """
         return np.load(self.path + '/{}/ids_{}.npy'.format(self.name, self.name))
+
+    ###########################---------------CALCULATIONS WITH MAHS-------------------#################################
+
+    def make_zxs(self, z, save=True, minsnapdist=5, jump_frac=[30, 25, 20, 10], snapnums=[11, 27, 59, 79, 89],
+                 fracs=[90, 75, 50, 10, 1]):
+
+        mahs = self.get_mah()
+        idxs = self.get_mah_ids()
+        reds = self.get_redshifts()
+        nhalos = len(mahs)
+        if nhalos != len(idxs):
+            raise ValueError("Problem with MAHs and Ids not the same length")
+        snapi = np.min(np.where(reds >= z))
+
+        mofz_snaps = np.where(np.array(snapnums) > snapi)[0]
+
+        if len(mofz_snaps) > 0:
+            mofz = True
+            numi = np.min(mofz_snaps)
+            rsnaps = snapnums[numi:]
+            mofzs = np.zeros((nhalos, len(rsnaps)))
+        else:
+            mofz = False
+
+        indices_i, gamma, beta, mass_i = [], [], [], []
+
+        zx_i = dict(zip(fracs, [[] for _ in range(len(fracs))]))
+        zmm_i = dict(zip(jump_frac, [[] for _ in range(len(jump_frac))]))
+
+        for i in range(len(mahs)):
+            mah = mahs[i][snapi:]
+            zeds = reds[snapi:len(mah) + snapi]
+            if len(mah) > minsnapdist:
+                m = mah[0]
+                if mofz:
+                    for k in range(len(rsnaps)):
+                        if len(mah) > rsnaps[k]:
+                            mofzs[i, k] = mah[rsnaps[k]] / m
+
+                immfracs = mah[:-minsnapdist] / mah[minsnapdist:] - 1
+                for p in range(len(jump_frac)):
+                    frc = jump_frac[p] / 100
+                    jump_idx = np.where(immfracs >= frc)[0]
+                    if len(jump_idx) > 0:
+                        res = np.sqrt(zeds[np.min(jump_idx)]*zeds[np.min(jump_idx)+minsnapdist])
+                        zmm_i[jump_frac[p]].append(res)
+                    else:
+                        zmm_i[jump_frac[p]].append(123)
+
+                for j in range(len(fracs)):
+                    frac = fracs[j] / 100
+                    ids = np.where(mah / m < frac)
+                    if len(ids[0]) > 0:
+                        idx = np.min(np.where(mah / m < frac))
+                        zx = np.sqrt(zeds[idx] * zeds[idx - 1])
+                        zx_i[fracs[j]].append(zx)
+                    else:
+                        zx_i[fracs[j]].append(np.inf)
+                try:
+                    param, cov = curve_fit(mcbride_mah, zeds, mah / m)
+                    gamma.append(param[0])
+                    beta.append(param[1])
+                except RuntimeError:
+                    gamma.append(-1e50)
+                    beta.append(-1e50)
+
+                mass_i.append(m)
+                indices_i.append(idxs[i])
+
+        mass_i, indices_i, gamma, beta = np.array(mass_i), np.array(indices_i, dtype=int), np.array(gamma), np.array(beta)
+
+        nameszx = ['z{}'.format(frac) for frac in fracs]
+        nameszmm = ['zmm{}'.format(jfrac) for jfrac in jump_frac]
+        namesmofz = ['M/M0(z={:1.1f})'.format(reds[rsnp]) for rsnp in rsnaps]
+        names = ['McBride_gamma', 'McBride_beta', 'Mass', 'Halo_index']
+
+        zxt = pd.DataFrame(zx_i)
+        zxt.columns = nameszx
+
+        zmmt = pd.DataFrame(zmm_i)
+        zmmt.columns = nameszmm
+
+        mofzt = pd.DataFrame(mofzs, columns=namesmofz)
+
+        tosave = dict(zip(names, [gamma, beta, mass_i, indices_i]))
+        otht = pd.DataFrame(tosave)
+
+        if save:
+            zxt.to_csv(self.path + '/{}/zxt_{}_z{}.dat'.format(self.name, self.name, z))
+            zmmt.to_csv(self.path + '/{}/zmmt_{}_z{}.dat'.format(self.name, self.name, z))
+            mofzt.to_csv(self.path + '/{}/mofzt_{}_z{}.dat'.format(self.name, self.name, z))
+            otht.to_csv(self.path + '/{}/otht_{}_z{}.dat'.format(self.name, self.name, z))
+        return zxt, zmmt, mofzt, otht
+
+    def get_agedata(self, z, atype='oth'):
+        return pd.read_csv(self.path + '/{}/{}t_{}_z{}.dat'.format(self.name, atype, self.name, z))
 
     def average_growth(self, zf, mmin=1e12, mmax=1e14, mbins=20, zbins=20, save=False):
         """Calculates the average growth over the last dynamical timescale of halos between mmin and mmax at zf"""
@@ -209,6 +348,7 @@ class Simulation:
                             .format(self.name, zf, self.name, mbins))
         return res_sim, ntot_sim, ps_sim
 
+    ###############-----------------MERGERS AND MERGER RATES SECTION--------------------################################
     def get_desc_prog(self, snap, wpos=False):
         """ Gets the descendant and progenitor masses if they are already saved"""
         mds = np.load(self.path + '/progs_desc/{}_desc_mass_snap{}.npy'.format(self.name, snap), allow_pickle=True)
@@ -218,7 +358,6 @@ class Simulation:
                                allow_pickle=True)  # Gets the prog positions if needed.
             return mds, mprg, prog_pos
         return mds, mprg
-
 
     def make_prog_desc(self, snapshot, usepos=False, save=True):
         """Makes a list of descendant masses and each corresponding prog mass"""
@@ -293,8 +432,6 @@ class Simulation:
             return np.array(mds, dtype=object), np.array(mprg, dtype=object), np.array(pos_prg, dtype=object)
         return np.array(mds, dtype=object), np.array(mprg, dtype=object)
 
-
-
     def dndxi(self, snap, mlim, bins=20, ximin=1e-2, ximax=1.0, wpos=False):
         if wpos:
             mds, mprg, prog_pos = self.get_desc_prog(snap, wpos)
@@ -317,7 +454,7 @@ class Simulation:
                 #     else:
                 #         tnds += np.max(pgmasses) * xis > mmin
 
-                tnds += xis > mmin/(mass - mmin)
+                tnds += xis > mmin / (mass - mmin)
                 if len(mprg[i]) > 1:
                     pgmasses = np.array(mprg[i])
                     if wpos:
@@ -360,8 +497,8 @@ class Simulation:
                     #     else:
                     #         tnds[:, k] += xilim < dexis
 
-                    xilim = 1/(mds[i]/mmin - 1)
-                    #print(xilim)
+                    xilim = 1 / (mds[i] / mmin - 1)
+                    # print(xilim)
                     tnds[:, k] += xilim < dexis
                     if len(mprg[i]) > 1:
                         pgmasses = mprg[i]
@@ -379,6 +516,8 @@ class Simulation:
                                     mres[:, k] += rat > dexis
 
         return mres, tnds
+
+    ############################-------------VARIOUS PLOTTING CHECKS, HMF, CONC-----------##############################
 
     def plot_tests(self, test, snaps, vol=500, h=0.7, conctype=1):
         f_halos = self.path + self.name + '/halos/'
@@ -451,56 +590,59 @@ class Simulation:
                 plt.show()
 
 
-##############---------------- Getting the merger rates from sims -----####################
 
-sim_names = ['M25S07', 'M25S08', 'M25S09', 'M03S07', 'M03S08', 'M03S09', 'M35S07', 'M35S08', 'M35S09',
-             'Illustris', 'bolshoiP', 'bolshoiW', 'M03S08b', 'm25s85', 'm2s8', 'm4s7', 'm4s8', 'm2s9',
-             'm3s8_50', 'm3s8', 'm35s75', 'm4s9', 'm3s9', 'm25s75', 'm2s1']
-omegas = [0.25, 0.25, 0.25, 0.3, 0.3, 0.3, 0.35, 0.35, 0.35, 0.309, 0.307, 0.27, 0.3, 0.25, 0.2, 0.4, 0.4, 0.2, 0.3
-    , 0.3, 0.35, 0.4, 0.3, 0.25, 0.2]
-sigmas = [0.7, 0.8, 0.9, 0.7, 0.8, 0.9, 0.7, 0.8, 0.9, 0.816, 0.82, 0.82, 0.8, 0.85, 0.8, 0.7, 0.8, 0.9, 0.8
-    , 0.8, 0.75, 0.9, 0.9, 0.75, 1.0]
+if __name__ == "__main__":
+    ##############---------------- Getting the merger rates from sims -----####################
 
-sims = dict(zip(sim_names, list(zip(omegas, sigmas))))
+    sim_names = ['M25S07', 'M25S08', 'M25S09', 'M03S07','M03S08', 'M03S09', 'M35S07', 'M35S08', 'M35S09',
+                 'Illustris', 'bolshoiP', 'bolshoiW', 'M03S08b', 'm25s85', 'm2s8', 'm4s7', 'm4s8', 'm2s9',
+                 'm3s8_50', 'm3s8', 'm35s75', 'm4s9', 'm3s9', 'm25s75', 'm2s1', 'm3s7', 'm3s85', 'm2s7', 'm25s8',
+                 'm35s8', 'm25s9', 'm35s85', 'm3s75', 'm35s9', 'm35s7']
+    omegas = [0.25, 0.25, 0.25, 0.3, 0.3, 0.3, 0.35, 0.35, 0.35, 0.309, 0.307, 0.27, 0.3, 0.25, 0.2, 0.4, 0.4, 0.2,  0.3
+              ,0.3, 0.35, 0.4, 0.3, 0.25, 0.2, 0.3, 0.3, 0.2, 0.25, 0.35, 0.25, 0.35, 0.3, 0.35, 0.35]
+    sigmas = [0.7, 0.8, 0.9, 0.7, 0.8, 0.9, 0.7, 0.8, 0.9, 0.816, 0.82, 0.82, 0.8, 0.85, 0.8, 0.7, 0.8, 0.9, 0.8
+              ,0.8, 0.75, 0.9, 0.9, 0.75, 1.0, 0.7, 0.85, 0.7, 0.8, 0.8, 0.9, 0.85, 0.75, 0.9, 0.7]
 
-mrate_path = '/home/painchess/asus_fedora/merger_rate'
-old_path = '/home/painchess/disq2/ahf-v1.0-101/'
-localpath = '/home/painchess/sims/'
-externalpath = '/home/painchess/mounted/HR_sims/niagara/'
-illustris_path = '/home/painchess/mounted/TNG300-625/output'
-
-#
-# s = -1
-# sim38 = Simulation(sim_names[s], omegas[s], sigmas[s], localpath)
-# reds = sim38.get_redshifts()
-
-
-# snapshots = [3, 5, 8, 15]
-# for snap in snapshots:
-#     nmgs, tnds = sim38.dndxi(snap, 1e13)
-#     red = reds[snap]
-#     dz = reds[snap+1]-reds[snap]
-#     ximin, ximax, resol = 1e-2, 1, 20
-#     xis = np.logspace(np.log10(ximin), np.log10(ximax), resol+1)
-#     dxis, ximeans = xis[1:]-xis[:-1], np.sqrt(xis[1:]*xis[:-1])
-#
-#     y = nmgs/dz/dxis/tnds[:-1]
-#     plt.loglog(ximeans,  y, 'o', color='C{}'.format(snap))
-#     plt.loglog(xis[1:-1], ell_mrate_per_n(5e13, red, xis, om0=omegas[s], sig8=sigmas[s]), color='C{}'.format(snap), linewidth=2)
-# plt.show()
-
-# # snaps = np.arange(0, 80, 40)
-# snaps = [0, 80]
-# # simselec = [-10, -5, -6, -12, -1]
-# simselec = [-1, -10, -5, -6, -12]
-# for s in simselec:
-#     sim38 = Simulation(sim_names[s], omegas[s], sigmas[s], localpath)
-#     reds = sim38.get_redshifts()
-#     # snaps = np.arange(0, 24, 8)
-#     # sim38.plot_tests('conc', snaps, vol=500)
-#     sim38.plot_tests('hmf', snaps, vol=500)
-# snaps = np.arange(80)
-# sim = 'm4s9'
-# sim1 = Simulation(sim, sims[sim][0], sims[sim][1], localpath)
-# for snap in snaps:
-#      sim1.make_prog_desc(snap)
+    # sims = dict(zip(sim_names, list(zip(omegas, sigmas))))
+    #
+    # mrate_path = '/home/painchess/asus_fedora/merger_rate'
+    # old_path = '/home/painchess/disq2/ahf-v1.0-101/'
+    # localpath = '/home/painchess/sims/'
+    # externalpath = '/home/painchess/mounted/HR_sims/niagara/'
+    # illustris_path = '/home/painchess/mounted/TNG300-625/output'
+    #
+    # #
+    # # s = -1
+    # # sim38 = Simulation(sim_names[s], omegas[s], sigmas[s], localpath)
+    # # reds = sim38.get_redshifts()
+    #
+    #
+    # # snapshots = [3, 5, 8, 15]
+    # # for snap in snapshots:
+    # #     nmgs, tnds = sim38.dndxi(snap, 1e13)
+    # #     red = reds[snap]
+    # #     dz = reds[snap+1]-reds[snap]
+    # #     ximin, ximax, resol = 1e-2, 1, 20
+    # #     xis = np.logspace(np.log10(ximin), np.log10(ximax), resol+1)
+    # #     dxis, ximeans = xis[1:]-xis[:-1], np.sqrt(xis[1:]*xis[:-1])
+    # #
+    # #     y = nmgs/dz/dxis/tnds[:-1]
+    # #     plt.loglog(ximeans,  y, 'o', color='C{}'.format(snap))
+    # #     plt.loglog(xis[1:-1], ell_mrate_per_n(5e13, red, xis, om0=omegas[s], sig8=sigmas[s]), color='C{}'.format(snap), linewidth=2)
+    # # plt.show()
+    #
+    # # snaps = np.arange(0, 80, 40)
+    # snaps = [0, 80]
+    # # simselec = [-10, -5, -6, -12, -1]
+    # simselec = ['m3s8', 'm2s7', 'm2s9', 'm4s9']
+    # for s in simselec:
+    #     sim38 = Simulation(s, sims[s][0], sims[s][1], localpath)
+    #     reds = sim38.get_redshifts()
+    #     # snaps = np.arange(0, 24, 8)
+    #     # sim38.plot_tests('conc', snaps, vol=500)
+    #     sim38.plot_tests('hmf', snaps, vol=500)
+    # # snaps = np.arange(80)
+    # # sim = 'm4s9'
+    # # sim1 = Simulation(sim, sims[sim][0], sims[sim][1], localpath)
+    # # for snap in snaps:
+    # #      sim1.make_prog_desc(snap)
